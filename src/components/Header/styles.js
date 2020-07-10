ee7f569789f4619223caf86c50575f2984a0fe31
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-10 14:37:41 -0300

    add vector icons

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-content: center;
+  padding: 40px 0 30px;
+`;
