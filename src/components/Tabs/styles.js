d26dbdf48783e731f6a7a5e9a926d2ba5bbec6d2
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-10 15:22:45 -0300

    add tabs

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.View`
   margin-top: 20px;
 `;
 
-export const TabContainer = styled.ScrollView.attrs({
+export const TabsContainer = styled.ScrollView.attrs({
   horizontal: true,
   contentContainerStyle: {paddingLeft: 10, paddingRight: 20},
   showsHorizontalScrollIndicator: false,
@@ -15,7 +15,7 @@ export const TabItem = styled.View`
   width: 100px;
   height: 100px;
   background: rgba(255, 255, 255, 0.2);
-  border-radius: 3;
+  border-radius: 3px;
   margin-left: 10px;
   padding: 10px;
   justify-content: space-between;
