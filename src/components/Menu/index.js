31412b7e3f58e0b5f9c5e88577a890c9178a2f66
Bruno Cardoso <bruno@liveonsolutions.com>
2020-07-20 04:22:32 -0300

    prepare structure from qrcode rendering

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-import {Container} from './styles';
+import QRCode from 'react-native-qrcode';
+
+import {Container, Code} from './styles';
 
 function Menu() {
-  return <Container />;
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="https://github.com/bhct"
+          size={80}
+          bgColor="#FFF"
+          fgColor="#8b10ae"
+        />
+      </Code>
+    </Container>
+  );
 }
 
 export default Menu;
